e4defaca40c3bce9c17160f21bf1fd09d97e6969
Gabriele Bavastro <gabrieleb84@hotmail.it>
2020-02-17 09:37:19 +0100

    Update Fase3.

---
+++ Fase3.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+public class Fase3 {
+  public static void main(String[] args) {
+		    HashMap<Character, Integer> fullName = new HashMap<Character, Integer>();
+		    fullName.put('B', 2);
+		    fullName.put('A', 4);
+		    fullName.put('V', 2);
+		    fullName.put('A', 4);
+			fullName.put('S', 1);
+		    fullName.put('T', 1);
+		    fullName.put('R', 2);
+		    fullName.put('O', 2);
+			fullName.put(' ', 3);
+			fullName.put('N', 2);
+		    fullName.put('A', 4);
+		    fullName.put('V', 2);
+		    fullName.put('O', 2);
+			fullName.put('N', 2);
+		    fullName.put('E', 3);
+			fullName.put(' ', 3);
+		    fullName.put('G', 1);
+		    fullName.put('A', 4);
+		    fullName.put('B', 2);
+			fullName.put('R', 2);
+		    fullName.put('I', 1);
+			fullName.put('E', 3);
+		    fullName.put('L', 1);
+		    fullName.put('E', 3);
+		    fullName.put(' ', 3);
+		    fullName.put('1', 1);
+		    fullName.put('9', 1);
+		    fullName.put('8', 1);
+		    fullName.put('4', 1);
+			System.out.println(fullName);
+		  }
+		}
